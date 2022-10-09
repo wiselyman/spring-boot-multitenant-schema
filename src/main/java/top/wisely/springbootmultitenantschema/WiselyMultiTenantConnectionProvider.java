dab6c72731dc0771a4e246a2446e59fc6a69f85e
Yunfei Wang <wiselyman2008@gmail.com>
2022-10-09 22:26:31 +0800

    some update,not finish

--- src/main/java/top/wisely/springbootmultitenantschema/WiselyMultiTenantConnectionProvider.java
+++ src/main/java/top/wisely/springbootmultitenantschema/WiselyMultiTenantConnectionProvider.java
@@ -1,22 +1,64 @@
 package top.wisely.springbootmultitenantschema;
 
 
-import org.hibernate.engine.jdbc.connections.spi.AbstractDataSourceBasedMultiTenantConnectionProviderImpl;
+import org.hibernate.engine.jdbc.connections.spi.MultiTenantConnectionProvider;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
 
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 
-public class WiselyMultiTenantConnectionProvider extends AbstractDataSourceBasedMultiTenantConnectionProviderImpl {
+@Component
+public class WiselyMultiTenantConnectionProvider implements MultiTenantConnectionProvider {
+
+    private final DataSource dataSource;
+
+    public WiselyMultiTenantConnectionProvider(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
 
     @Override
-    protected DataSource selectAnyDataSource() {
-        return null;
+    public Connection getAnyConnection() throws SQLException {
+        return dataSource.getConnection();
     }
 
     @Override
-    protected DataSource selectDataSource(String tenantIdentifier) {
-        return null;
+    public void releaseAnyConnection(Connection connection) throws SQLException {
+        connection.close();
+    }
+
+    @Override
+    public Connection getConnection(String tenantIdentifier) throws SQLException {
+        String schema = tenantIdentifier;
+        final Connection connection = getAnyConnection();
+        connection.setSchema(schema);
+        return connection;
+    }
+
+    @Override
+    public void releaseConnection(String tenantIdentifier, Connection connection) throws SQLException {
+        connection.setSchema("public");
+        connection.close();
+    }
+
+
+    @Override
+    public boolean supportsAggressiveRelease() {
+        return false;
+    }
+
+    @Override
+    public boolean isUnwrappableAs(Class<?> unwrapType) {
+        return false;
+    }
+
+    @Override
+    public <T> T unwrap(Class<T> unwrapType) {
+        throw new UnsupportedOperationException("Can't unwrap this.");
     }
 }
--- src/main/java/top/wisely/springbootmultitenantschema/WiselyTenantIdResolver.java
+++ src/main/java/top/wisely/springbootmultitenantschema/WiselyTenantIdResolver.java
@@ -1,9 +1,11 @@
 package top.wisely.springbootmultitenantschema;
 
 import org.hibernate.context.spi.CurrentTenantIdentifierResolver;
+import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
+@Component
 public class WiselyTenantIdResolver implements CurrentTenantIdentifierResolver {
     private static final ThreadLocal<String> CURRENT_TENANT = new ThreadLocal<>();
     public void setCurrentTenant(String currentTenant) {
@@ -12,7 +14,7 @@ public class WiselyTenantIdResolver implements CurrentTenantIdentifierResolver {
 
     @Override
     public String resolveCurrentTenantIdentifier() {
-        return Optional.ofNullable(CURRENT_TENANT.get()).orElse("unknown");
+        return Optional.ofNullable(CURRENT_TENANT.get()).orElse("public");
     }
 
     @Override

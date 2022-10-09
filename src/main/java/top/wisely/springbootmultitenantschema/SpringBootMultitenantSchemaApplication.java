dd5bb715f81204f6a1828fb5359140b68d21182c
Yunfei Wang <wiselyman2008@gmail.com>
2022-10-09 16:49:48 +0800

    Initial commit

---
+++ src/main/java/top/wisely/springbootmultitenantschema/SpringBootMultitenantSchemaApplication.java
@@ -0,0 +1,13 @@
+package top.wisely.springbootmultitenantschema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMultitenantSchemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMultitenantSchemaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/top/wisely/springbootmultitenantschema/SpringBootMultitenantSchemaApplicationTests.java
@@ -0,0 +1,13 @@
+package top.wisely.springbootmultitenantschema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMultitenantSchemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
